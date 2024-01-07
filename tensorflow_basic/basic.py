ce04ed357c5b89e57ab583379145172e055e9452
JangAJang <janghee5395@gmail.com>
2024-01-07 16:32:14 +0900

    feat: 텐서플로우 기본 함수 정리

---
+++ tensorflow_basic/basic.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+tensor1 = tf.constant([4, 5, 6])
+tensor2 = tf.constant([7, 8, 9])
+
+# tf.add() 더하기
+# tf.subtract() 빼기
+# tf.divide() 나누기
+# tf.multiply() 곱하기
+# tf.matmul() 행렬 곱하기
+print(tensor1)
